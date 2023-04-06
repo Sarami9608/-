dc42a7985ccfbe4925835f16c8cda83b4b99fab5
young <youngjae9683@gmail.com>
2023-04-06 19:54:27 +0900

    230406

--- DB/ConcentVO.py
+++ DB/ConcentVO.py
@@ -1,11 +1,15 @@
 class ConcentVO():
     __conID = ""
+    __p_date = ''
     __energy = 0
     __p_state = 0
     def __init__(self,conID):
         self.__conID = conID
     def getConID(self):
         return self.__conID
+    
+    def getDate(self):
+        return self.__p_date
     
     def getEnergy(self):
         return self.__energy
