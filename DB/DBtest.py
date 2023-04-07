2fd6b4ace9a9e6add055d6918dcd769e82715615
YOUNG <youngjae9683@gmail.com>
2023-04-07 17:29:07 +0900

    delete

--- DB/DBtest.py
+++
@@ -1,8 +0,0 @@
-import ConcentDAO2
-
-
-dao = ConcentDAO2.ConcentDAO()
-
-concentVO = {"CONID":'A0001' , "p_date":'sysdate' , "energy":0, "p_state":0}
-dao.insert_Energy(concentVO)
-# dao.insert_Predict([concentVO])
