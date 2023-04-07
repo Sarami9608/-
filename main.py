dba755cabe9dc1e324506799e6487c86b57adfdb
young <youngjae9683@gmail.com>
2023-04-07 17:22:11 +0900

    230407 오늘의 핵심 프로젝트

--- main.py
+++ main.py
@@ -37,5 +37,21 @@
 #           사용자에게 알림을 데이터 베이스에 전송합니다.
 #           해당 콘센트 전원을 차단합니다.
 #
+from MQTT import realMqtt
+from DB import ConcentDAO
+i = 0
+mqttList = []
+rows = ['a0001','a0002','a0003']
+dao = ConcentDAO.ConcentDAO()
 
-
+while True:
+    # TODO 특정 시간에 동작을 한다.
+        if dao.count_concent_ids() == False:
+            rows = dao.get_member_concent_ids()
+            for row in rows:
+                mqttList.append(realMqtt.MQTTClient(row))
+                print(mqttList[i].relay_topic)
+                print(mqttList[i].energy_topic)
+                print(mqttList)
+                i +=1
+# TODO 
