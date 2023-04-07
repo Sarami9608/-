9439d25c1cf8c2f3c71b0ee35be9c653ef55003d
young <youngjae9683@gmail.com>
2023-04-07 12:52:26 +0900

    230407

--- mqtt/mqttRelay.py
+++ mqtt/mqttRelay.py
@@ -7,11 +7,13 @@ broker_port = 8883
 mqtt_user = ""
 mqtt_password = ""
 mqtt_topic = "MQTT_Topic"
-
+data = {
+    "conID": "A001",
+    "relayValue": 0
+}
 # MQTT 클라이언트 생성
 client = mqtt.Client()
 client.username_pw_set(mqtt_user, mqtt_password)
-relayValue = 1 
 # MQTT 브로커 연결
 client.connect(broker_address, broker_port)
 
@@ -27,10 +29,7 @@ client.on_message = on_message
 while True:
     # 현재 시간을 메시지로 전송
     # 데이터 생성
-    data = {
-        "conID": "A001",
-        "relayValue": 0
-    }
+
     if data['relayValue'] == 0:
         data['relayValue'] = 1
     else:
@@ -38,7 +37,8 @@ while True:
     # JSON 형식으로 데이터 전송
     payload = json.dumps(data)
     client.publish("conRelay", payload)
-    print("published")
+    print(f"published : {data['relayValue']}")
+    time.sleep(60)
 # MQTT 메시지 수신 대기
 # publish도 진행하고자 한다면 스레드 분리가 필요
-    client.loop_forever()
+client.loop_forever()
